cdab6ece05092d3b198bb9573c80530f9e33e710
msvalandro <msvalandro@proton.me>
2024-08-15 16:05:22 +0100

    test: finish dashboard tests

---
+++ src/screens/Dashboard/Dashboard.spec.tsx
@@ -0,0 +1,71 @@
+import { mockCityAPIResponse } from '@__tests__/mocks/api/mockCityApiResponse'
+import { mockWeatherAPIResponse } from '@__tests__/mocks/api/mockWeatherAPIResponse'
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@__tests__/utils/customRender'
+import { saveStorageCity } from '@libs/asyncStorage/cityStorage'
+import { Dashboard } from '@screens/Dashboard'
+import { api } from '@services/api'
+
+describe('Screen: Dashboard', () => {
+  it('should be show city weather', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse })
+
+    const city = {
+      id: '1',
+      name: 'Rio do Sul, BR',
+      latitude: 123,
+      longitude: 456,
+    }
+
+    await saveStorageCity(city)
+
+    render(<Dashboard />)
+
+    const cityName = await waitFor(() => screen.findByText(/rio do sul/i))
+    expect(cityName).toBeTruthy()
+  })
+
+  it('should be show another selected weather city', async () => {
+    const city = {
+      id: '1',
+      name: 'Rio do Sul, BR',
+      latitude: 123,
+      longitude: 456,
+    }
+
+    await saveStorageCity(city)
+
+    jest
+      .spyOn(api, 'get')
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+      .mockResolvedValueOnce({ data: mockCityAPIResponse })
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+
+    render(<Dashboard />)
+
+    await waitForElementToBeRemoved(() => screen.queryByTestId('loading'))
+
+    const cityName = 'São Paulo'
+
+    await waitFor(() =>
+      act(() => {
+        const search = screen.getByTestId('search-input')
+        fireEvent.changeText(search, cityName)
+      }),
+    )
+
+    await waitFor(() =>
+      act(() => {
+        fireEvent.press(screen.getByText(cityName, { exact: false }))
+      }),
+    )
+
+    expect(screen.getByText(cityName, { exact: false })).toBeTruthy()
+  })
+})
