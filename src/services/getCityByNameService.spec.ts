7d70c2407ebfed518b50df5a5bb58aea08f925b2
msvalandro <msvalandro@proton.me>
2024-08-15 15:01:24 +0100

    test: add async storage mocks

--- src/services/getCityByNameService.spec.ts
+++ src/services/getCityByNameService.spec.ts
@@ -1,4 +1,4 @@
-import { mockCityAPIResponse } from '@__tests__/mocks/mockCityApiResponse'
+import { mockCityAPIResponse } from '@__tests__/mocks/api/mockCityApiResponse'
 
 import { api } from './api'
 import { getCityByNameService } from './getCityByNameService'
